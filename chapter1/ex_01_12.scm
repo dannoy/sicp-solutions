6da29aa40c2f43ac8db4d1adc52c954cd8642041
dannoy.lee <dannoy.lee@gmail.com>
2012-10-14 22:41:16 +0800

    add ex_01_12.scm

---
+++ chapter1/ex_01_12.scm
@@ -0,0 +1,24 @@
+;;; 不过此时行和列均为1开始
+(define (pascal row col)
+  (cond ((or (> col row) 
+	     (< col 1)) 0)
+	((or (= col 1)
+	     (= col row)) 1)
+	(else (+ (pascal (- row 1) (- col 1))
+		 (pascal (- row 1) col))
+	      
+	      )
+   )
+  )
+
+  ;;; 另外一种方式是将pascal(row,col)视为
+  ;;; 二项式的系数，所以可以用C k
+  ;;;                           n,来求
+  ;;; 即为n!/(k! *(n-k)!)
+  ;;; 不过此时行和列均为0开始
+
+  (define (pascal-norec row col)
+    (/ (factorial row)
+       (* (factorial col)
+          (factorial (- row col))))
+  )
